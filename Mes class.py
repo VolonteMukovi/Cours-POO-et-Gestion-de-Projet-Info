74756d165a0ffc41738c78319f483167a839dbd1
volonteMukovi <mukovivolonte@gmail.com>
2024-05-17 13:22:17 +0200

    creation du constructeur et attribut

--- Mes class.py
+++ Mes class.py
@@ -2,6 +2,15 @@ from abc import ABC
 
 """Class Abstrait"""
 class Fichier(ABC):
-    
+    def __init__(self,image=None,video=None,audion=None,text=None,word=None,pdf=None):
+        self.__image = image
+        self.__video = video
+        self.__audion = audion
+        self.__text = text
+        self.__word = word
+        self.__pdf = pdf
+
+        
+
 
        
