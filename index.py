fe8b06b987100ff32519184ba449b7bf7c9aa14b
volonteMukovi <mukovivolonte@gmail.com>
2024-05-27 13:06:15 +0200

    ajout du CV

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import filedialog
-from classes.Mes_class import ExtensionFileOrganizer
+from Classe.Mes_class import ExtensionFileOrganizer
 from customtkinter import *
 
 
@@ -17,8 +17,8 @@ framegauche.pack(side=LEFT,padx=5)
 framedroite = tk.Frame(window,width=400,height=800,bg="white")
 framedroite.pack_propagate(0)
 framedroite.pack(side=RIGHT,padx=5)
-mo_titre=Label(framedroite,text="BIENVENUE SUR FILES ORGANIZER",bg="black",fg="white",width="30")
-mo_titre.place(x='110',y='20')
+mo_titre=CTkLabel(master=framedroite,text="BIENVENUE SUR FILES ORGANIZER",font=("arial",20),text_color="blue")
+mo_titre.place(relx=0.5,rely=0.25,anchor="center")
 
 def select_folder():
     folder_path = filedialog.askdirectory()
@@ -29,13 +29,14 @@ def organization():
     organize = ExtensionFileOrganizer()
     organize.organiser(folder_path)
     
+    
 texte = tk.Text(framegauche,wrap="word",bg="blue",fg="white")
 texte.insert('1.0','    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum accusamus corporis maiores eos incidunt error quae totam autem ad fugiat voluptatum, sapiente labore neque aut amet. Perspiciatis tempora vero repellat     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum accusamus corporis maiores eos incidunt error quae totam autem ad fugiat voluptatum, sapiente labore neque aut amet. Perspiciatis tempora vero repellat??')
 texte.tag_configure('center',justify="center")
 texte.tag_add("center",'1.0','end')
 texte.pack(expand=True)
 
-button = CTkButton(master=framedroite,text="Selectionner un Dossier a Organisant",corner_radius=32,fg_color="#C850C0",hover_color="#4158D0",command=organization)
+button = CTkButton(master=framedroite,text="Selectionner un Dossier a Organisant",corner_radius=32,fg_color="#4158D0",hover_color="#000000",command=organization)
 button.place(relx=0.5,rely=0.5,anchor="center")
 # button_org.place(x='160',y='250')
 # (framedroite,text="Selectionner un Dossier a Organisant",bg="blue",fg="white",command=organization)
